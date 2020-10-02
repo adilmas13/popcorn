287ec7a9fe47db326b3d86a0bdc5d619554129b6
Adil Shaikh <adil@invideo.io>
2020-10-02 19:41:27 +0530

    feat: splash to home activity

--- app/src/main/kotlin/com/popcorn/ui/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/popcorn/ui/splash/SplashActivity.kt
@@ -1,12 +1,21 @@
 package com.popcorn.ui.splash
 
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.popcorn.R
 import com.popcorn.base.BaseActivity
 import com.popcorn.databinding.ActivitySplashBinding
+import com.popcorn.ui.home.MainActivity
+import com.popcorn.utilities.screenWidth
 import com.popcorn.utilities.setTransparentStatusBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding, SplashActivityViewModel>() {
@@ -15,8 +24,15 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashActivityViewMod
     override fun onCreate(savedInstanceState: Bundle?) {
         setTransparentStatusBar()
         super.onCreate(savedInstanceState)
-        animator = SplashActivityAnimator(tvPop, tvCorn, ivLogo).apply { init() }
-        animator.startAnimation()
+        animator = SplashActivityAnimator(tvPop, tvCorn, ivLogo, screenWidth()).apply { init() }
+        zip2(viewModel.onSuccess, animator.startAnimation(), timer()).observe(
+            this,
+            {
+                animator.endAnimation {
+                    redirectToHome()
+                }
+            }
+        )
         viewModel.getConfig()
     }
 
@@ -25,15 +41,52 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashActivityViewMod
     override fun subscribeToObservers() {
         val ctx = this
         viewModel.apply {
-            onSuccess.observe(ctx, { redirectToHome() })
             onError.observe(ctx, { showMessage("Something went wrong") })
         }
     }
 
+    private fun timer(): LiveData<Boolean> {
+        val liveData = MutableLiveData<Boolean>()
+        lifecycleScope.launch {
+            delay(1000)
+            liveData.postValue(true)
+        }
+        return liveData
+    }
+
     private fun redirectToHome() {
-//        startActivity(MainActivity.getIntent(this))
-//        finish()
+        startActivity(MainActivity.getIntent(this))
+        finish()
     }
 
     override fun createViewModel() = SplashActivityViewModel::class.java
+}
+
+fun <T1, T2, T3> zip2(
+    src1: LiveData<T1>,
+    src2: LiveData<T2>,
+    src3: LiveData<T3>
+): MediatorLiveData<Boolean> {
+    val mediatorLiveData = MediatorLiveData<kotlin.Boolean>()
+    var check = 0
+
+    fun update() {
+        if (++check == 3) {
+            mediatorLiveData.postValue(true)
+        }
+    }
+
+    mediatorLiveData.addSource(src1) {
+        Log.d("THE_SOURCE", "SOURCE_1")
+        update()
+    }
+    mediatorLiveData.addSource(src2) {
+        Log.d("THE_SOURCE", "SOURCE_2")
+        update()
+    }
+    mediatorLiveData.addSource(src3) {
+        Log.d("THE_SOURCE", "SOURCE_3")
+        update()
+    }
+    return mediatorLiveData
 }
--- app/src/main/kotlin/com/popcorn/ui/splash/SplashActivityAnimator.kt
+++ app/src/main/kotlin/com/popcorn/ui/splash/SplashActivityAnimator.kt
@@ -3,13 +3,17 @@ package com.popcorn.ui.splash
 import android.animation.AnimatorSet
 import android.view.View
 import android.view.animation.OvershootInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.popcorn.utilities.animate
 import com.popcorn.utilities.onRendered
 
 class SplashActivityAnimator(
     private val popText: View,
     private val cornText: View,
-    private val logo: View
+    private val logo: View,
+    private val screenWidth: Int
 ) {
 
     fun init() {
@@ -26,44 +30,92 @@ class SplashActivityAnimator(
         }
     }
 
-    fun startAnimation() {
+    fun startAnimation(): LiveData<Boolean> {
+        val test = MutableLiveData<Boolean>()
         logo.onRendered {
             val animationSet = AnimatorSet()
-            val logoBuilder = logo.animate {
-                scale {
-                    scaleX = 1f
-                    scaleY = 1f
-                }
-                alpha {
-                    alpha = 1f
-                }
-            }.build()
-            val popTextBuilder = popText.animate {
-                translate {
-                    translateXFrom = (-popText.width).toFloat()
-                }
-                alpha {
-                    alpha = 1f
-                }
-            }.build()
-            val cornTextBuilder = cornText.animate {
-                startDelay = 100
-                translate {
-                    translateXFrom = (-cornText.width).toFloat()
-                }
-                alpha {
-                    alpha = 1f
-                }
-            }.build()
             animationSet.playTogether(
-                logoBuilder.animator,
-                popTextBuilder.animator,
-                cornTextBuilder.animator
+                logoEnterAnimation().animator,
+                popTextEnterAnimation().animator,
+                cornTextEnterAnimation().animator
             )
             animationSet.interpolator = OvershootInterpolator()
             animationSet.duration = 500L
             animationSet.startDelay = 1000
+            animationSet.doOnEnd {
+                test.value = true
+            }
             animationSet.start()
         }
+        return test
+    }
+
+    private fun logoEnterAnimation() = logo.animate {
+        scale {
+            scaleX = 1f
+            scaleY = 1f
+        }
+        alpha {
+            alpha = 1f
+        }
+    }.build()
+
+    private fun cornTextEnterAnimation() = cornText.animate {
+        startDelay = 100
+        translate {
+            translateXFrom = (-cornText.width).toFloat()
+        }
+        alpha {
+            alpha = 1f
+        }
+    }.build()
+
+    private fun popTextEnterAnimation() = popText.animate {
+        translate {
+            translateXFrom = (-popText.width).toFloat()
+        }
+        alpha {
+            alpha = 1f
+        }
+    }.build()
+
+    fun endAnimation(onCompleted: () -> Unit) {
+        val animationSet = AnimatorSet()
+        val logoBuilder = logo.animate {
+            scale {
+                scaleX = 0f
+                scaleY = 0f
+            }
+            alpha {
+                alpha = 0f
+            }
+        }.build()
+        val popTextBuilder = popText.animate {
+            translate {
+                translateXTo = screenWidth.toFloat()
+            }
+            alpha {
+                alpha = 0f
+            }
+        }.build()
+        val cornTextBuilder = cornText.animate {
+            startDelay = 100
+            translate {
+                translateXTo = (-cornText.width).toFloat()
+            }
+            alpha {
+                alpha = 0f
+            }
+        }.build()
+        animationSet.playTogether(
+            logoBuilder.animator,
+            popTextBuilder.animator,
+            cornTextBuilder.animator
+        )
+        animationSet.interpolator = OvershootInterpolator()
+        animationSet.duration = 1000L
+        animationSet.startDelay = 3000
+        animationSet.doOnEnd { onCompleted() }
+        animationSet.start()
     }
 }
