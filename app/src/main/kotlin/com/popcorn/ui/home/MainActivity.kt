3c8c41a1b22c5c401c5242032a3bfc50883fd32c
Adil Shaikh <adil@invideo.io>
2020-10-04 00:09:57 +0530

    feat: added main activity header

--- app/src/main/kotlin/com/popcorn/ui/home/MainActivity.kt
+++ app/src/main/kotlin/com/popcorn/ui/home/MainActivity.kt
@@ -3,12 +3,10 @@ package com.popcorn.ui.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AppCompatDelegate
 import com.popcorn.R
 import com.popcorn.base.BaseActivity
 import com.popcorn.databinding.ActivityMainBinding
+import com.popcorn.utilities.setTransparentStatusBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,28 +18,8 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>() 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTransparentStatusBar()
         super.onCreate(savedInstanceState)
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = getString(R.string.app_name)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.theme -> toggleTheme()
-        else -> super.onOptionsItemSelected(item)
-    }
-
-    private fun toggleTheme(): Boolean {
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
-        return true
     }
 
     override fun createViewModel() = MainActivityViewModel::class.java
