4447764005202b170b19d7a16b1d0fe95e80feb1
Adil Shaikh <adil@invideo.io>
2020-12-30 18:32:29 +0530

    fix: data store imports

--- data/src/main/kotlin/com/popcorn/data/storage/DataStoreLocalStorage.kt
+++ data/src/main/kotlin/com/popcorn/data/storage/DataStoreLocalStorage.kt
@@ -1,9 +1,9 @@
 package com.popcorn.data.storage
 
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
 import com.popcorn.domain.LocalStorage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
