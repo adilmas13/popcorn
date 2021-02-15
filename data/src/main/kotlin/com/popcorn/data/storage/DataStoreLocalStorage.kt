979ea7be3abe033a8b9566fa15ca71f887718b92
Adil Shaikh <adil@invideo.io>
2021-02-15 07:50:40 +0530

    fix: preferences key compilation issue

--- data/src/main/kotlin/com/popcorn/data/storage/DataStoreLocalStorage.kt
+++ data/src/main/kotlin/com/popcorn/data/storage/DataStoreLocalStorage.kt
@@ -3,7 +3,7 @@ package com.popcorn.data.storage
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.popcorn.domain.LocalStorage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -15,13 +15,13 @@ class DataStoreLocalStorage @Inject constructor(
 
     override suspend fun setString(key: String, value: String) {
         dataStore.edit {
-            val prefKey = preferencesKey<String>(key)
+            val prefKey = stringPreferencesKey(key)
             it[prefKey] = value
         }
     }
 
     override suspend fun getString(key: String, default: String): Flow<String> {
-        val prefKey = preferencesKey<String>(key)
+        val prefKey = stringPreferencesKey(key)
         return dataStore.data.map { it[prefKey] ?: default }
     }
 }
